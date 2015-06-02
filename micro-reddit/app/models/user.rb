32bfb183d9502a9710f188d820a24e405a8621ed
Luchillo <luchillo17@gmail.com>
2015-06-02 09:03:50 -0500

    Added user name index in migration for uniq and better validations.

--- micro-reddit/app/models/user.rb
+++ micro-reddit/app/models/user.rb
@@ -5,7 +5,8 @@ class User < ActiveRecord::Base
 	before_save{ self.email.downcase! }
 
 	validates :name,	presence: true,
-										length: {maximum: 50}
+										uniqueness: true,
+										length: {minimum: 3, maximum: 50}
 	validates :email, presence:true,
 										uniqueness: {case_sensitive: false},
 										length: {maximum: 255},
--- micro-reddit/db/schema.rb
+++ micro-reddit/db/schema.rb
@@ -25,5 +25,6 @@ ActiveRecord::Schema.define(version: 20150602031347) do
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["name"], name: "index_users_on_name", unique: true, using: :btree
 
 end
