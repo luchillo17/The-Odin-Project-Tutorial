6cc988e0c1d897c105d304ece23e55236fad7a79
Luchillo <luchillo17@gmail.com>
2015-06-07 13:36:09 -0500

    Changed structure of User model file as recommended by rails style guide: https://github.com/bbatsov/rails-style-guide

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -2,19 +2,18 @@ class User < ActiveRecord::Base
 
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
-	before_save{ self.email.downcase! }
-
 	validates :name,	presence: true,
 										length: {maximum: 50}
 	validates :email, presence:true,
 										uniqueness: {case_sensitive: false},
 										length: {maximum: 255},
 										format: {with: VALID_EMAIL_REGEX}
-
-	has_secure_password
 	validates :password,	presence: true,
 												length: {minimum: 6}
+
+	has_secure_password
 	 
+	before_save{ self.email.downcase! }
 
 	def save(*args)
 		super
