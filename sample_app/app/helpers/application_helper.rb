a8e87f61996f24d2ae8a522b3d1727839b0c37d8
Luchillo <luchillo17@gmail.com>
2015-06-11 12:55:38 -0500

    Restart Tutorial.

--- sample_app/app/helpers/application_helper.rb
+++ sample_app/app/helpers/application_helper.rb
@@ -1,11 +1,2 @@
 module ApplicationHelper
-	# Function for returning full title depending on page.
-	def full_title(page_title='')
-		base_title = "Ruby on Rails Tutorial Sample App"
-		if page_title.empty?
-			base_title
-		else
-			"#{page_title} | #{base_title}"
-		end
-	end
 end
